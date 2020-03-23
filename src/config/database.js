1d1a9e18566ccb5977706d8174ceb79bdb78b3af
jthiagopereira <jthiagopereira@gmail.com>
2020-03-23 02:50:55 -0300

    Configurando Sequelize

---
+++ src/config/database.js
@@ -0,0 +1,12 @@
+module.exports = {
+  dialect: 'postgres',
+  host: 'localhost',
+  username: 'postgres',
+  password: 'thiago',
+  database: 'gobarber',
+  difine: {
+    timestamps: true,
+    underscored: true,
+    underscoredAll: true,
+  },
+};
