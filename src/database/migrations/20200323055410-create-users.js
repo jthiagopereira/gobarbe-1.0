093807302846b2fce7738c0a3e12bd4d177ee210
jthiagopereira <jthiagopereira@gmail.com>
2020-03-23 16:13:47 -0300

    Correção arquivo database.js

--- src/database/migrations/20200323055410-create-users.js
+++ src/database/migrations/20200323055410-create-users.js
@@ -1,11 +1,11 @@
 module.exports = {
-  up: (queryInterface, Sequelize) => {
-    return queryInterface.createTable('users', {
+  up: (queryInterface, Sequelize) =>
+    queryInterface.createTable('users', {
       id: {
         type: Sequelize.INTEGER,
-        allowNull: false,
-        autoIncrement: true,
-        primarykey: true,
+        allowNull: false, // não permite nulo
+        autoIncrement: true, // auto encremento
+        primarykey: true, // chave primaria ou seja não muda.
       },
       name: {
         type: Sequelize.STRING,
@@ -14,15 +14,16 @@ module.exports = {
       email: {
         type: Sequelize.STRING,
         allowNull: false,
-        unique: true,
+        unique: true, // Não pode ter email repetidos
       },
-      passowrd_hash: {
+      password_hash: {
         type: Sequelize.STRING,
         allowNull: false,
       },
       provider: {
+        // Tanto pode ser o cliente como o admistrador de serviços.
         type: Sequelize.BOOLEAN,
-        defaultValue: false,
+        defaultValue: false, // quando for prestador sera "true" quando não "false"
         allowNull: false,
       },
       created_at: {
@@ -33,10 +34,6 @@ module.exports = {
         type: Sequelize.DATE,
         allowNull: false,
       },
-    });
-  },
-
-  down: (queryInterface) => {
-    return queryInterface.dropTable('users');
-  },
+    }),
+  down: (queryInterface) => queryInterface.dropTable('users'),
 };
